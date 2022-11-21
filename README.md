5969890ea2e67bba333e2317e13e276e90defdf2
Austin Papritz <austin@papritz.dev>
2022-11-20 17:09:58 -0800

    all three button eventlisteners, adds to arr and logs to console

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # README
 
-If the user clicks buttons 1-3 in order, background turns red
+1. If the user clicks buttons 1-3 in order, background turns red
+2. If any button is pressed, background toggles red (cannot put in each eventlistener)
--- app.js
+++ app.js
@@ -7,15 +7,31 @@ const btn3 = document.getElementById('3');
 const clear = document.getElementById('clear');
 
 /* State */
-let arr = [1];
+let arr = [];
 
 /* Events */
 
-btn1.addEventListener('click', () => {});
+btn1.addEventListener('click', () => {
+    arr.push(1);
+    console.log(arr);
+});
+
+btn2.addEventListener('click', () => {
+    arr.push(2);
+    console.log(arr);
+});
+
+btn3.addEventListener('click', () => {
+    arr.push(3);
+    console.log(arr);
+});
 
 clear.addEventListener('click', () => {
     arr = [];
 });
+
+// Functions
+
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
