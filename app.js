abd4b49f72f918db9c4e109bd7e31473fc4d6e09
Austin Papritz <austin@papritz.dev>
2022-11-20 16:57:02 -0800

    clear arr and btn1 eventlistener

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 /* Imports */
 
 /* Get DOM Elements */
+const btn1 = document.getElementById('1');
+const btn2 = document.getElementById('2');
+const btn3 = document.getElementById('3');
+const clear = document.getElementById('clear');
 
 /* State */
+let arr = [1];
 
 /* Events */
 
+btn1.addEventListener('click', () => {});
+
+clear.addEventListener('click', () => {
+    arr = [];
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
