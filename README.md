cd99eb926e035eb3f6b85d922f5ba077ae1c50f5
Austin Papritz <austin@papritz.dev>
2022-11-20 17:47:20 -0800

    body changes red via classList.add

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # README
 
 1. If the user clicks buttons 1-3 in order, background turns red
-2. If any button is pressed, background toggles red (cannot put in each eventlistener)
+2. If any button is pressed, background toggles blue (cannot put in each eventlistener)
--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const btn2 = document.getElementById('2');
 const btn3 = document.getElementById('3');
 const clear = document.getElementById('clear');
 
+const body = document.querySelector('body');
+
 /* State */
 let arr = [];
 
@@ -13,25 +15,35 @@ let arr = [];
 
 btn1.addEventListener('click', () => {
     arr.push(1);
+    bgRed();
     console.log(arr);
 });
 
 btn2.addEventListener('click', () => {
     arr.push(2);
+    bgRed();
     console.log(arr);
 });
 
 btn3.addEventListener('click', () => {
     arr.push(3);
+    bgRed();
     console.log(arr);
 });
 
 clear.addEventListener('click', () => {
     arr = [];
+    bgRed();
 });
 
 // Functions
-
+function bgRed() {
+    if (arr === (1, 2, 3)) {
+        body.classList.add('red');
+    } else {
+        document.body.style.background = '';
+    }
+}
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
